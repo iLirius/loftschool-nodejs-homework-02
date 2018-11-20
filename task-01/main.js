859e656e8d87261c413da4c9ef676ba9834bf7f9
Gavrilov Ivan <gavrilov.iv.spb@mail.ru>
2018-11-20 15:03:37 +0300

    Добавляет  в 1 задании прием консольных параметров

--- task-01/main.js
+++ task-01/main.js
@@ -1,35 +1,49 @@
 #!/usr/bin/env node
 
 const http = require('http');
+var path = require('path');
+const optimist = require('optimist');
+var scriptName = path.basename(__filename);
 
-const port = 3000;
+const argv = optimist.usage(`Usage: ${scriptName} [options]`)
+  .demand(['i', 't'])
+  .options('i', {
+    alias: 'interval'
+  }).describe('i', 'интервал повторения сообщения в консоли [num ms]')
+  .options('t', {
+    alias: 'timeout'
+  }).describe('t', 'время через которое отобразить сообщение веб клиенту [num ms]')
+  .options('p', {
+    alias: 'port'
+  }).describe('p', 'http-server listen port')
+  .default('p', 3000)
+  .argv;
 
 console.log('Starting');
-
 const requestHandler = (request, response) => {
   if (request.url !== '/favicon.ico') {
     response.writeHead(404, {
       'Content-type': 'text/plain; charset=utf-8'
     });
 
-    let intrvalId = setInterval(() => {
-      console.log(new Date());
-    }, 1000);
+    let intervallId = setInterval(() => {
+      console.log(new Date().toString());
+    }, argv.interval);
 
     setTimeout(() => {
-      clearInterval(intrvalId);
+      clearInterval(intervallId);
       response.write(new Date().toString());
       response.end();
-    }, 10000);
+    }, argv.timeout);
 
   } else {
     response.end();
   }
 };
 
-http.createServer(requestHandler).listen(port, (err) => {
+http.createServer(requestHandler).listen(argv.port, (err) => {
   if (err) {
     return console.log('something bad happened', err);
   }
-  console.log(`Server is listening on ${port}`);
+  console.log(`Server is listening on ${argv.port}`);
 });
